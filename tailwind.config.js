7c26983a9eb65b222d0f0c78a9b70d927b9e5b97
Augustus Backup <evilkim25@gmail.com>
2022-09-17 10:21:54 +0800

    調整 tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./content/**/*.{html,js,md}", "./layouts/**/*.{html,md}", "./layouts_pug/**/*.pug", "./static/js/**.js"],
+  content: ["./content/**/*.{html,js,md}", "./layouts/**/*.{html,md}", "./layouts_pug/**/*.pug", "./static/js/**.js", "./assets/js/**.js"],
   theme: {
     extend: {
       fontFamily: {
